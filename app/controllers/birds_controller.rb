69514a35efe83ab18b6e4ab06726be381b1c7714
Stellanyambura <stellagitonga.com>
2023-03-17 10:30:29 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -10,8 +10,13 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    if bird.valid?
+      render json: bird, status: :created
+    else
+      render json: { errors: bird.errors }, status: :unprocessable_entity
+    end
   end
+
 
   # GET /birds/:id
   def show
